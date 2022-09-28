750e090597dc4f7337ccaecca5ad999fc280bc9b
Sebastian <seba.laca@gmail.com>
2022-09-28 10:46:40 +0200

    Update app

--- js/app.js
+++ js/app.js
@@ -28,16 +28,47 @@ function iniciarApp() {
 //Valida formulario
 function validarFormulario(e) {
     if(e.target.value.length > 0) {
-        console.log('Hay algo');
+
+        //Elimina los errores - limpio formulario
+        const error = document.querySelector('p.error')
+        if (error !== null) {
+            error.remove()
+        }
+
+        e.target.classList.remove('border', 'border-red-500')
+        e.target.classList.add('border', 'border-green-500')
     }else {
+        e.target.classList.remove('border', 'border-green-500')
         e.target.classList.add('border', 'border-red-500')
-        mostrarError()
+        mostrarError('Todos los campos son obligatorios')
+    }
+
+    if (e.target.type === 'email') {
+        // Expresion regular para valñidar email
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+        //Con este if comprobamos si el email es valido
+        if (er.test(e.target.value)) {
+
+        //Elimina los errores - limpio formulario
+            const error = document.querySelector('p.error')
+            if (error !== null) {
+                error.remove()
+            }
+
+            e.target.classList.remove('border', 'border-red-500')
+            e.target.classList.add('border', 'border-green-500')
+        }else{
+            e.target.classList.remove('border', 'border-green-500')
+            e.target.classList.add('border', 'border-red-500')
+            mostrarError('Email no valido')
+        }
     }
 }
 
-function mostrarError() {
+function mostrarError(mensaje) {
     const mensajeError = document.createElement('p')
-    mensajeError.textContent = 'Todos los campos son obligatorios'
+    mensajeError.textContent = mensaje
     mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error')
 
     const errores = document.querySelectorAll('.error')
