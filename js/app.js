e0e0c55e9f4b631476d5058ba84451e214ee6ee6
Sebastian <seba.laca@gmail.com>
2022-09-28 09:56:48 +0200

    validar formularios

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 //VARIABLES
 const btnEnviar = document.querySelector('#enviar')
 
+//VARIABLES PARA CAMPOS
+const email = document.querySelector('#email')
+const asunto = document.querySelector('#asunto')
+const mensaje = document.querySelector('#mensaje')
+
 eventListeners()
 function eventListeners() {
+    //Cuando la app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp)
+
+    //Campos del formulario
+    email.addEventListener('blur', validarFormulario)
+    asunto.addEventListener('blur', validarFormulario)
+    mensaje.addEventListener('blur', validarFormulario)
 }
 
 //FUNCIONES
@@ -11,4 +22,13 @@ function eventListeners() {
 function iniciarApp() {
     btnEnviar.disabled = true
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50')
+}
+
+//Valida formulario
+function validarFormulario(e) {
+    if(e.target.value.length > 0) {
+        console.log('Hay algo');
+    }else {
+        e.target.classList.add('border', 'border-red-500')
+    }
 }
